4428cea3db4960099ea52ac4879741b4386e7686
kfirnaftali <48950327+kfirnaftali@users.noreply.github.com>
2024-03-18 12:43:32 +0200

    Delete qrcode directory

--- qrcode/qrcode_generator.py
+++
@@ -1,6 +0,0 @@
-import qrcode
-img = qrcode.make("https://cloud.google.com/blog/products/data-analytics/learn-how-to-get-real-time-llm-insights-using-dataflow")
-# img = qrcode.make("https://tinyurl.com/vertex-ai-vector-search")
-img.save("gcp_blog_post.png")
-
-
